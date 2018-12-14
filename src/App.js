88e1039e4b177945af7179a0da24e6f65364c9c1
rosco1010 <doll3098@ou.edu>
2018-12-14 10:56:48 -0600

    Instantiate the app with the initial gql query

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import './App.css';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const MAIN_QUERY = gql`
+  {
+    pizzaSizes {
+      name
+      maxToppings
+      toppings {
+        topping {
+          name
+          price
+        }
+        defaultSelected
+      }
+      basePrice
+    }
+  }
+`;
+
 
 class App extends Component {
   render() {
@@ -14,6 +34,16 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <h1>Waldo Photos Pizza Cart</h1>
+          <Query query={MAIN_QUERY}>
+            {
+              ({ loading, error, data }) => {
+                if (loading) return <div>Fetching</div>;
+                if (error) return <div>Error</div>;
+                console.log(data);
+                return <div>Success!</div>
+              }
+            }
+          </Query>
         </div>
       </Provider>
     );
