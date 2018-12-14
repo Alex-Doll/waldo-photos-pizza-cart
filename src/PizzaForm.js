7a4c36ba2ae52ce2ada108abd0499775eff7c935
rosco1010 <doll3098@ou.edu>
2018-12-14 14:02:59 -0600

    Have the toppings be controlled by the form component

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -7,16 +7,50 @@ class PizzaForm extends Component {
     super(props);
 
     this.state = {
-      size: null
+      size: null,
+      toppings: []
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this._isLessThanMaxToppings = this._isLessThanMaxToppings.bind(this);
+    this.handleToppingsChange = this.handleToppingsChange.bind(this);
   }
 
   handleChange(event) {
+    const defaultToppings = this.props.pizzaSizes.filter(pizzaSize => pizzaSize.name === event.target.value)[0].toppings.filter(topping => topping.defaultSelected);
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
+      toppings: defaultToppings
     });
+  }
+
+  _isLessThanMaxToppings() {
+    const pizzaData = this.props.pizzaSizes.filter(size => size.name === this.state.size)[0];
+    if (pizzaData.maxToppings === null) return true;
+
+    return this.state.toppings.length < pizzaData.maxToppings;
+  }
+
+  handleToppingsChange(event) {
+    let checkbox = event.target;
+    const pizzaData = this.props.pizzaSizes.filter(size => size.name === this.state.size)[0];
+    const topping = pizzaData.toppings.filter(topping => topping.topping.name === checkbox.value)[0];
+
+      if (checkbox.checked) {
+      if (this._isLessThanMaxToppings()) {
+        this.setState(prevState => ({
+          toppings: [...prevState.toppings, topping]
+        }));
+      }
+      else {
+        checkbox.checked = false;
+      }
+    }
+    else {
+      this.setState(prevState => ({
+        toppings: prevState.toppings.filter(oldTopping => JSON.stringify(oldTopping) !== JSON.stringify(topping))
+      }));
+    }
   }
 
 
@@ -40,7 +74,7 @@ class PizzaForm extends Component {
         <fieldset onChange={this.handleChange}>
           {pizzaPicker}
         </fieldset>
-        { this.state.size && <ToppingsPicker maxToppings={pizzaData.maxToppings} toppings={pizzaData.toppings} /> }
+        { this.state.size && <ToppingsPicker maxToppings={pizzaData.maxToppings} toppings={pizzaData.toppings} handleChange={this.handleToppingsChange} selectedToppings={this.state.toppings.map(topping => topping.topping.name)} /> }
       </div>
     );
   }
--- src/ToppingsPicker.js
+++ src/ToppingsPicker.js
@@ -2,58 +2,17 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class ToppingsPicker extends Component {
-  constructor(props) {
-    super(props);
-
-    const defaultToppings = this.props.toppings.filter(topping => topping.defaultSelected).map(topping => topping.topping.name);
-    
-    this.state = {
-      toppings: defaultToppings
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this._isLessThanMaxToppings = this._isLessThanMaxToppings.bind(this);
-  }
-
-  _isLessThanMaxToppings() {
-    if (this.props.maxToppings === null) return true;
-
-    return this.state.toppings.length < this.props.maxToppings;
-  }
-
-  handleChange(event) {
-    let checkbox = event.target;
-    const toppingName = event.target.value;
-
-    if (checkbox.checked) {
-      if (this._isLessThanMaxToppings()) {
-        this.setState(prevState => ({
-          toppings: [...prevState.toppings, toppingName]
-        }));
-      }
-      else {
-        checkbox.checked = false;
-      }
-    }
-    else {
-      this.setState(prevState => ({
-        toppings: prevState.toppings.filter(topping => topping !== toppingName)
-      }));
-    }
-    
-  }
-
   render() {
     const toppingsCheckboxes = this.props.toppings.map((topping, index) => {
       return (<div key={index}>
-        <input type='checkbox' id={topping.topping.name} value={topping.topping.name} name='toppings' defaultChecked={topping.defaultSelected} onChange={this.handleChange} />
-        <label htmlFor={topping.topping.name}>{topping.topping.name}</label>
+        <input type='checkbox' id={topping.topping.name} value={topping.topping.name} name='toppings' checked={this.props.selectedToppings.includes(topping.topping.name)} onChange={this.props.handleChange} />
+        <label htmlFor={topping.topping.name}>{topping.topping.name}{topping.defaultSelected ? 'TRUE' : 'FALSE'}</label>
       </div>);
     });
     
     return (
       <div>
-        <h2>Max Toppings {this.props.maxToppings}</h2>
+        <h2>Max Toppings {this.props.maxToppings === null ? 'Inf' : this.props.maxToppings}</h2>
         <fieldset>
           {toppingsCheckboxes}
         </fieldset>
@@ -64,7 +23,8 @@ class ToppingsPicker extends Component {
 
 ToppingsPicker.propTypes = {
   maxToppings: PropTypes.number,
-  toppings: PropTypes.array.isRequired
+  toppings: PropTypes.array.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default ToppingsPicker;
