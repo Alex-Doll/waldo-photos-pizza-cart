b62d7f945f76ccd037cb6e0367006ea73d99416d
rosco1010 <doll3098@ou.edu>
2018-12-14 10:26:13 -0600

    Add the initial setup of the redux store

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './App.css';
 
 class App extends Component {
   render() {
+    const rootReducer = (state, action) => {
+      return state;
+    };
+    const initialState = {};
+    const store = createStore(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
     return (
+      <Provider store={store}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Waldo Photos Pizza Cart</h1>
       </div>
+      </Provider>
     );
   }
 }
